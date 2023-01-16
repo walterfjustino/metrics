7125bca2dc26d81f843c55bfdc8bba130e0f9870
walterfjustino <walterfjustino@gmail.com>
2023-01-16 10:47:55 -0300

    feature: remove unused imports

--- src/main/java/com/metric/app/AppApplication.java
+++ src/main/java/com/metric/app/AppApplication.java
@@ -1,11 +1,7 @@
 package com.metric.app;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 @SpringBootApplication
 public class AppApplication  {
